7d98d58a814ad2f85357fd00874a88b007833ab5
Harendra4077 <121235789+Harendra4077@users.noreply.github.com>
2024-07-04 13:39:29 +0530

    done

---
+++ db/dbconnection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+exports.connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb://localhost:27017");
+    // const conn = await mongoose.connect(process.env.DB_URL);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Database not connected", error);
+  }
+};
+
